3a87dc8e2345ce82de83c6f0685322bc83c399db
Vincent Trumpff <vincent@stoic.com>
2017-11-30 17:24:15 +0100

    added arch management

--- alpine/mylife-home-core-plugins/build.sh
+++ alpine/mylife-home-core-plugins/build.sh
@@ -2,8 +2,15 @@
 
 plugin_name=$1
 plugin_version=$2
-plugin_dependencies=$3
+plugin_noarch=$3
+plugin_dependencies=$4
 build_dir="$plugin_name-$plugin_version"
+
+if [ "$plugin_noarch" == "yes" ] then
+  plugin_arch="noarch"
+else
+  plugin_arch="$(apk --print-arch)"
+fi
 
 rm -rf $build_dir
 mkdir $build_dir
@@ -11,5 +18,9 @@ cd $build_dir
 cp ../APKBUILD.template ./APKBUILD
 sed -i "s/{{plugin-name}}/$plugin_name/g" APKBUILD
 sed -i "s/{{plugin-version}}/$plugin_version/g" APKBUILD
+sed -i "s/{{plugin-platform}}/$plugin_arch/g" APKBUILD
 sed -i "s/{{plugin-dependencies}}/$plugin_dependencies/g" APKBUILD
+
+echo "building $plugin_name-$plugin_version (arch=$plugin_arch, dependencies=$plugin_dependencies)"
+
 abuild -r
--- alpine/mylife-home-core-plugins/generate.sh
+++ alpine/mylife-home-core-plugins/generate.sh
@@ -2,13 +2,13 @@
 
 target_plugin=$1
 
-while IFS=, read -r plugin_name plugin_version plugin_dependencies
+while IFS=, read -r plugin_name plugin_version plugin_noarch plugin_dependencies
 do
   if ! [ -z "$target_plugin" ] && [ "$target_plugin" != "$plugin_name" ]
   then
     continue;
   fi
   echo "GENERATING PLUGIN : $plugin_name"
-  ./build.sh "$plugin_name" "$plugin_version" "$plugin_dependencies"
-  echo "PLUGIN GENERATED : $plugin_name"
+  ./build.sh "$plugin_name" "$plugin_version" "$plugin_noarch" "$plugin_dependencies"
+  echo "PLUGIN GENERATED : $plugin_name (noarch:$plugin_noarch)"
 done < plugin-list
