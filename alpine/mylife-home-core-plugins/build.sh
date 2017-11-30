b0cb0331e81e8f0c01a0dfd8430e65383d07d195
Vincent Trumpff <vincent@stoic.com>
2017-11-30 16:12:51 +0100

    update

--- alpine/mylife-home-core-plugins/build.sh
+++ alpine/mylife-home-core-plugins/build.sh
@@ -5,10 +5,11 @@ plugin_version=$2
 plugin_dependencies=$3
 build_dir="$plugin_name-$plugin_version"
 
+rm -rf $build_dir
 mkdir $build_dir
 cd $build_dir
 cp ../APKBUILD.template ./APKBUILD
-sed -i 's/{{plugin-name}}/$plugin_name/g' APKBUILD
-sed -i 's/{{plugin-version}}/$plugin_version/g' APKBUILD
-sed -i 's/{{plugin-dependencies}}/$plugin_dependencies/g' APKBUILD
+sed -i "s/{{plugin-name}}/$plugin_name/g" APKBUILD
+sed -i "s/{{plugin-version}}/$plugin_version/g" APKBUILD
+sed -i "s/{{plugin-dependencies}}/$plugin_dependencies/g" APKBUILD
 abuild -r
