64687d49715dd484ed93244395864edef05da899
vincent-tr <vincent@stoic.com>
2018-01-09 16:26:54 +0100

    cleanup

--- alpine/gpio-admin/abuild.sh
+++ alpine/gpio-admin/abuild.sh
@@ -6,7 +6,9 @@ tmp_dir=/tmp/rpi-image-builder-output
 abuild_subdir=$(basename $(dirname $(dirname $script)))
 
 mkdir -p $tmp_dir
+
 abuild checksum
 abuild -r -P $tmp_dir
 
 cp $tmp_dir/$abuild_subdir/armhf/*.apk $target_dir
+rm -rf $tmp_dir
