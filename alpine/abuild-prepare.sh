d1bbbb9b27cbbb9611fe7c57eeef7cf1a3b87948
vincent-tr <vincent@stoic.com>
2018-01-11 18:01:52 +0100

    need to reload after addgroup

--- alpine/abuild-prepare.sh
+++ alpine/abuild-prepare.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 # packages
-sudo apk add --no-cache --virtual .build-utils alpine-sdk
+apk add --no-cache --virtual .build-utils alpine-sdk
+
+home=/home/builder
+rmount_build=$home/alpine-build-home-resources
+
+sudo -i -u builder /bin/sh - << eof
 
 # prepare for abuild
 sudo addgroup builder abuild
 sudo mkdir -p /var/cache/distfiles
 sudo chmod a+w /var/cache/distfiles
-mkdir -p $HOME/.abuild
-cp $HOME/alpine-build-home-resources/abuild/* $HOME/.abuild
+mkdir -p $home/.abuild
+cp $rmount_build/abuild/* $home/.abuild
+
+eof
