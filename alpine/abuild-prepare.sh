7e52ac6df63a85b9d7ecafddcf1c543a870a1f49
vincent-tr <vincent@stoic.com>
2018-01-11 17:57:16 +0100

    fix dirs

--- alpine/abuild-prepare.sh
+++ alpine/abuild-prepare.sh
@@ -8,4 +8,4 @@ sudo addgroup builder abuild
 sudo mkdir -p /var/cache/distfiles
 sudo chmod a+w /var/cache/distfiles
 mkdir -p $home/.abuild
-cp $rmount_build/abuild/* $home/.abuild
+cp $HOME/alpine-build-home-resources/abuild/* $HOME/.abuild
