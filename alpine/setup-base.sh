33869d7006832e9cd97926c0fa00b7cbbd99f5af
vincent-tr <vincent@stoic.com>
2018-01-11 15:54:01 +0100

    fix

--- alpine/setup-base.sh
+++ alpine/setup-base.sh
@@ -35,7 +35,7 @@ sudo addgroup builder abuild
 sudo mkdir -p /var/cache/distfiles
 sudo chmod a+w /var/cache/distfiles
 mkdir -p $home/.abuild
-cp $rmount/abuild/* $home/.abuild
+cp $rmount_build/abuild/* $home/.abuild
 
 # prepare git repo
 cd ~
