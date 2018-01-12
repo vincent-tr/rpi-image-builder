129cd83ab4d31f9b930a897f5573c30f936cf6f9
vincent-tr <vincent@stoic.com>
2018-01-12 08:44:13 +0100

    sudo mount

--- alpine/build-linux-modloops.sh
+++ alpine/build-linux-modloops.sh
@@ -47,9 +47,9 @@ build_modloop() {
   # rpi1
   mkdir -p /tmp/modloop-rpi
   mkdir -p /tmp/new-modloop-rpi
-  mount /media/mmcblk0p1/boot/modloop-rpi /tmp/modloop-rpi -t squashfs -o loop
+  sudo mount /media/mmcblk0p1/boot/modloop-rpi /tmp/modloop-rpi -t squashfs -o loop
   cp -r /tmp/modloop-rpi/* /tmp/new-modloop-rpi
-  umount /tmp/modloop-rpi
+  sudo umount /tmp/modloop-rpi
   rmdir /tmp/modloop-rpi
   mkdir -p /tmp/new-modloop-rpi/modules/$version-rpi/extra
   cp -r $extra_rpi_dir/* /tmp/new-modloop-rpi/modules/$version-rpi/extra
@@ -63,9 +63,9 @@ build_modloop() {
   # rpi2
   mkdir -p /tmp/modloop-rpi2
   mkdir -p /tmp/new-modloop-rpi2
-  mount /media/mmcblk0p1/boot/modloop-rpi2 /tmp/modloop-rpi2 -t squashfs -o loop
+  sudo mount /media/mmcblk0p1/boot/modloop-rpi2 /tmp/modloop-rpi2 -t squashfs -o loop
   cp -r /tmp/modloop-rpi2/* /tmp/new-modloop-rpi2
-  umount /tmp/modloop-rpi2
+  sudo umount /tmp/modloop-rpi2
   rmdir /tmp/modloop-rpi2
   mkdir -p /tmp/new-modloop-rpi2/modules/$version-rpi2/extra
   cp -r $extra_rpi2_dir/* /tmp/new-modloop-rpi2/modules/$version-rpi2/extra
