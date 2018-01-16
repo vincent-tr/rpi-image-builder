31199817c315a382030bbf8c38d0f860f66f1a88
vincent-tr <vincent@stoic.com>
2018-01-16 11:48:34 +0100

    add prints

--- alpine/abuild-deploy.sh
+++ alpine/abuild-deploy.sh
@@ -8,10 +8,12 @@ arch=$(apk --print-arch)
 mkdir -p $target_dir/$arch
 mkdir -p $target_dir/noarch
 
+echo "copy $source_dir/APKINDEX.tar.gz to $target_dir/$arch"
 cp $source_dir/APKINDEX.tar.gz $target_dir/$arch
 
 for file in $source_dir/*.apk
 do
   package_arch=$(tar -zxOf $file .PKGINFO | grep -oE "arch = (.*)" | sed 's/arch = //g')
+  echo "copy $file to $target_dir/$package_arch"
   cp $file $target_dir/$package_arch
 done
