9c9511da1660ca332a2bdd3c80f19803d3e0542c
vincent-tr <vincent@stoic.com>
2018-01-25 16:20:18 +0100

    oops

--- alpine/mylife-home-core/configure.sh
+++ alpine/mylife-home-core/configure.sh
@@ -7,14 +7,13 @@ else
   flavor=-$1
 fi
 
-# script=$(readlink -f "$0")
-# base_dir=$(dirname $script)
-base_dir=~/workspace/private/rpi-image-builder/alpine/mylife-home-core
+script=$(readlink -f "$0")
+base_dir=$(dirname $script)
 
 echo "CONFIGURING FLAVOR : $flavor"
 
-#sed "s/{{flavor}}/$flavor/g" $base_dir/APKBUILD.template > $base_dir/APKBUILD
-#sed "s/{{flavor}}/$flavor/g" $base_dir/mylife-home-core.post-install.template > $base_dir/mylife-home-core.post-install
-#sed "s/{{flavor}}/$flavor/g" $base_dir/mylife-home-core.pre-deinstall.template > $base_dir/mylife-home-core.pre-deinstall
-#sed "s/{{flavor}}/$flavor/g" $base_dir/storage-service.initd.template > $base_dir/storage-service.initd
-#sed "s/{{flavor}}/$flavor/g" $base_dir/storage-service.sh.template > $base_dir/storage-service.sh
+sed "s/{{flavor}}/$flavor/g" $base_dir/APKBUILD.template > $base_dir/APKBUILD
+sed "s/{{flavor}}/$flavor/g" $base_dir/mylife-home-core.post-install.template > $base_dir/mylife-home-core.post-install
+sed "s/{{flavor}}/$flavor/g" $base_dir/mylife-home-core.pre-deinstall.template > $base_dir/mylife-home-core.pre-deinstall
+sed "s/{{flavor}}/$flavor/g" $base_dir/storage-service.initd.template > $base_dir/storage-service.initd
+sed "s/{{flavor}}/$flavor/g" $base_dir/storage-service.sh.template > $base_dir/storage-service.sh
