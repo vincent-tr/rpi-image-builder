50d7d0efaa2290a740dd8925b5daae684d61d387
vincent-tr <vincent@stoic.com>
2018-01-25 18:36:32 +0100

    fix path

--- alpine/mylife-home-inspircd-hub-config/gen-links.sh
+++ alpine/mylife-home-inspircd-hub-config/gen-links.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat links.list | while read -r host
+cat /etc/inspircd/links.list | while read -r host
 do
   echo "<link name=\"$host.&networkname;\" ipaddr=\"$host.&networkname;\" port=\"7000\" allowmask=\"*\" timeout=\"60\" sendpass=\"password\" recvpass=\"password\">"
 done
